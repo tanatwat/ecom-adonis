7abf3735af9ccb46ff5e0477bd85db22796e8bf4
Tanatwat Wattanasakul <tanatwat.w@gmail.com>
2019-04-30 13:40:27 +0700

    add code check before create

--- app/Controllers/Http/Admin/Promotion/CodeController.js
+++ app/Controllers/Http/Admin/Promotion/CodeController.js
@@ -1,23 +1,34 @@
 'use strict'
 
 const Database = use('Database')
+const Code = use('App/Models/Code')
 
 class CodeController {
    async get ({request}) {
       return await Database.table('codes').where('client_id', request.header('Client'))
    }
 
-   async store({request}) {
+   async store({request, response}) {
+
       const {code, value , type} = request.all()
 
-      await Database.table('codes').insert({
-         client_id: request.header('Client'),
-         code: code,
-         value: value,
-         type: type
-      })
+      const check = await Database.table('codes')
+      .where('client_id', request.header('Client'))
+      .where('code', code)
+      .first()
+      // Chedck if code exists
+      if(!check) {
+         const created = await Database.table('codes').insert({
+            client_id: request.header('Client'),
+            code: code,
+            value: value,
+            type: type
+         })
+         response.send({id: created[0]})
+      } else {
+         response.status(406).send('โค๊ดไม่สามารถซ้ำกันได้')
+      }
 
-      return 'success'
    }
 
    async destroy({request, params}) {
